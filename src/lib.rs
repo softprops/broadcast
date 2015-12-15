255dda4671daff0c861a3efbee0bc9b965bfafc6
softprops <d.tangren@gmail.com>
2015-12-15 12:58:30 +0900

    test something

--- src/lib.rs
+++ src/lib.rs
@@ -27,6 +27,19 @@ impl<A: Write, B: Write> Write for BroadcastWriter<A, B> {
     }
 }
 
-#[test]
-fn it_works() {
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::io::Write;
+    #[test]
+    fn broadcast() {
+        let mut first = Vec::new();
+        let mut second = Vec::new();
+        {
+            let mut broadcaster = BroadcastWriter::new(&mut first, &mut second);
+            let _ = broadcaster.write(b"it's over 9000!");
+        }
+        assert_eq!(first, second);
+    }
 }
