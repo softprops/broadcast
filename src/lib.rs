fcb18794ee2ef99665778144a9373f3bae25613b
softprops <d.tangren@gmail.com>
2015-12-15 12:04:49 +0900

    use new fn

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,18 @@
 use std::io::{Result, Write};
 
-/// Creates a new broadcast instance which can be used as a Write
-/// All data will be written to the primary writer as well as the seconardary
-/// writer. Errors that occur during the either write operartion will be yielded.
-pub fn broadcast<A: Write, B: Write>(primary: A, secondary: B) -> Broadcast<A, B> {
-    Broadcast { primary: primary, secondary: secondary }
-}
-
-/// A type which delegates
-///
+/// A type which duplicates its writes to all provided writers
 pub struct Broadcast<A, B> {
     primary: A,
     secondary: B,
+}
+
+impl<A: Write, B: Write> Broadcast<A,B> {
+    /// Creates a new broadcast instance which can be used as a Write
+    /// All data will be written to the primary writer as well as the seconardary
+    /// writer. Errors that occur during the either write operartion will be yielded.
+    pub fn new(primary: A, secondary: B) -> Broadcast<A, B> {
+        Broadcast { primary: primary, secondary: secondary }
+    }
 }
 
 impl<A: Write, B: Write> Write for Broadcast<A, B> {
